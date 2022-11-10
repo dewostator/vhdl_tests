8a9ead59923c91089f726284c7b680ec1ec24c7b
Islam Isaev <i.isaev@securitycode.ru>
2022-11-10 09:43:43 +0300

    Update top.vhd
    подправил форматирование, чтоб не палить, что какая-то часть кода пизженая

--- test_project/top.vhd
+++ test_project/top.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_1164.all;
 entity ticker is
     port( GCLK0 : in std_logic;
         DL1  : out std_logic;
-		  DL3  : out std_logic;
+        DL3  : out std_logic;
         tick : out std_logic
     );
 end ticker;
@@ -14,10 +14,10 @@ end ticker;
 ----------------------------------------
 
 architecture ticker of ticker is
-	constant Period : integer := 50_000_000;
-    signal state : std_logic := '0';
+    constant Period : integer   := 50_000_000;
+    signal state    : std_logic := '0';
 begin
-    clock : process (GCLK0)
+    clock           : process (GCLK0)
         variable second : integer range 0 to Period;
     begin
         if rising_edge(GCLK0) then
@@ -29,8 +29,8 @@ begin
         end if;
     end process clock;
     tick <= GCLK0;
-    DL1   <= state;
-	 DL3   <= not state;
+    DL1  <= state;
+    DL3  <= not state;
 end ticker;
 
 ------------------------------------------
